08e0da4943c6191609cd266395553b4b9794201e
Mitlyy <juravelivi@gmail.com>
2023-03-04 14:41:05 +0300

    -

--- 7.py
+++ 7.py
@@ -7,20 +7,25 @@ import scienceplots
 plt.style.use(['science', 'notebook', 'grid', 'Solarize_Light2'])
 
 dots = 4000
-_ = np.linspace(-1, 1, dots)
+ab = 1
+cd = 1
+_ = np.linspace(-ab, cd, dots)
 
-x, y = np.meshgrid(_,_)
+x, y = np.meshgrid(_, _)
 
-func = 2 * x**2 + y**2 + 4
+func = 2 * x ** 2 + y ** 2 + 4
 func = np.select([(x ** 2 + y ** 2 <= 1), True], [func, 0])
-rn = np.random.random((dots,dots)) * 6
+rn = np.random.random((dots, dots)) * func.max()
 rn = np.select([rn < func, True], [1, 0])
 
+
 def one(f, d = dots):
-	ans = 4/d**2 * sum(sum(f))
-	ans2 = 4*6 * sum(sum(rn))/dots**2
+	ans = (ab + cd) * (ab + cd) / d ** 2 * sum(sum(f))
+	ans2 = (ab + cd) * (ab + cd) * func.max() * sum(sum(rn)) / dots ** 2
 	print(np.around(ans, 3))
 	print(np.around(ans2, 3))
+
+
 one(func)
 
 # plt.contourf(x, y, func)
