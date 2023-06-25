6907d423ab4904ed546b2187560a4deccb276eec
Mitlyy <juravelivi@gmail.com>
2023-06-25 16:08:38 +0300

    -

--- 5.py
+++ 5.py
@@ -1,98 +1,125 @@
 import numpy as np
-from matplotlib import pyplot as plt
-import math
+import matplotlib.pyplot as plt
 import scienceplots
-from scipy.fftpack import *
 from numpy import loadtxt
-plt.style.use(['science', 'notebook', 'grid', 'Solarize_Light2'])
+from math import *
+from scipy.fftpack import *
+import scipy.integrate as integrate
+import scipy.special as special
 
+plt.style.use(['science', 'notebook', 'grid'])
 
+fig, ax = plt.subplots(2, 2, figsize=(8,8))
 data = loadtxt(r"C:\Users\User\Desktop\epsdata.txt", comments="#", delimiter=" ", unpack=True)
-E = data[1]
+E_tr = data[1]
 t = data[0]
 
-w_pl = 0.5e+12
-G = 0.1e+12
-w1 = 2 * np.pi * 1e+12
-F1 = 1
-f0 = 1e+12
-tau_p = 0.5e-12
-z = 1e-3
-d = 0.5e-3
-w0 = 2 * np.pi * f0
-c = 3e+8
-k0 = w0/c
 
-omega = np.linspace(1e+12, 12e+12, t.shape[0])
-eps_ = []
-N = []
-F = []
-T = []
-Etr = []
-w = np.linspace(w0-w0/2, w0, 100)
+eps = 1e-7
+eps1 = 1e-9
+dc = 10 / 3
+f0 = 1.0
+taup = 0.5
+delf = 0.6
+fmin = f0 - delf
+fmax = f0 + delf
+z = 1.0
+ompl = 1.0
+gam = 0.5
+tz = t - (z/3)*10
+omega = np.linspace(fmin, fmax, 1024)
 
-def F(w_):
-    # print(-(w_-w_0)**2 * tau_p**2)
-    return 2 * np.sqrt(np.pi) * tau_p * np.exp(-(w_-w0)**2 * tau_p**2)
+E_in = 2 * np.cos(2 * np.pi * f0 * tz-4) * np.exp(-(tz-4)**2 / 4 / taup**2)
 
-plt.plot(w, F(w))
+ax[0, 0].plot(t, E_in)
+
+F = fftshift(fft(E_in))
+# ax[1, 0].plot(omega, abs(F))
+ax[0, 1].plot(t, E_tr)
+
 
 
 def T(w_):
     dt = t[1]-t[0]
     a = []
     for i in range(t.shape[0]):
-        if F(w_)[i] != 0:
-            # print(F(w_)[i])
-            a.append(1/F(w_)[i] * sum(np.exp(1j * w_[i] * (t - z / c)) * E) * dt)
-        else:
-            a.append(0)
+        a.append(sum(np.exp(2*np.pi*1j*w_[i]*(t-z/3*10)) * E_tr * dt))
     ans = np.asarray(a)
     return ans
 
+T_ = T(omega)
+ax[1, 0].plot(omega, T_.real)
+ax[1, 0].plot(omega, T_.imag)
+
+
+
+
+eps = 1e-7
+eps1 = 1e-9
+dc = 10 / 3
+f0 = 1.0
+taup = 0.5
+delf = 0.6
+fmin = f0 - delf
+fmax = f0 + delf
+z = 1.0
+ompl = 1.0
+gam = 0.5
+maxt = 300
+th = 1 / 20
+tin = -1.
+maxf=512
+New=20
+fh=2*delf/maxf
+n=1
+fa1=[]
+maxt=1024
+maxtd2=maxt//2
+Tfm = np.zeros(1024) * 1j
+epsm = np.zeros(1024) * 1j
+for mf in range(1, maxf+2):
+    f=fmin+(mf-1)*fh
+    fa1.append(f)
+    sum4=0
+    sum2=0
+    t=tin+th
+    for m in range(1, maxtd2):
+        m4=m*2-1
+        sum4=sum4+E_tr[m4-1]*np.exp(2*np.pi*1j*f*(t-z/3*10))
+        t=t+th
+        m2=m*2
+        sum2=sum2+E_tr[m2-1]*np.exp(2*np.pi*1j*f*(t-z/3*10))
+        t=t+th
+    Tf= (4*sum4+2*sum2) * th/3 * exp((2*pi*taup)**2*(f-f0)**2)/(2*sqrt(pi)*taup)
+    Tfm[mf-1] = Tf
+
+# ax[1, 1].plot(omega, Tfm.imag)
+# ax[1, 1].plot(omega, Tfm.real)
 def newton():
+	def f(n_):
+		g = 4 * n_ / ((n_+1)**2 * np.exp(-1j*n_* 2 * pi * omega * dc)-(n_-1)**2 * np.exp(1j*n_* 2 * pi * omega * dc)) - T_
+		return g
+	def df(n_, h = 0.001):
+		dg = (f(n_ + h) - f(n_)) / h
+		return dg
+	def iter(i = 0, xn = 1):
+		xn_ = xn - f(xn) / df(xn)
+		# print(xn_[0])
+		if i == 3:
+			return xn_
+		else:
+			i += 1
+			ans = iter(i, xn = xn_)
+			return ans
+		return xn_
+	ans = iter(0, xn = 1)
+	return ans
 
-    def f(n_):
-        g = 4 * n_ / ((n_+1)**2 * np.exp(-1j*k0*n_*d)-(n_-1)**2 * np.exp(1j*k0*n_*d)) - T(omega)
-        # plt.plot(omega, g.real)
-        return g
-
-    def df(n_, h = 0.001):
-        dg = (f(n_ + h) - f(n_)) / h
-        return dg
-
-    def iter(func, i = 0, xn = 1):
-        xn_ = xn - f(xn) / df(xn)
-        # print(xn_)
-        if i == 2:
-            return xn_
-        else:
-            i += 1
-            ans = iter(func, i, xn = xn_)
-            return ans
-        return xn_
-
-    ans = iter(func = f(1), xn = 1)
-    return ans
-# n = newton()
-
-
-def eps():
-    omega1 = 2 * np.pi * 1e+12
-    G_1 = 0.5e+12
-    omega_pl = 1.5e+12
-    b = omega1**2 - omega**2
-    return 1 + omega_pl**2 * (b/(b**2 + omega**2 * G_1) + 1j * (G_1* omega)/(b**2 + omega**2 * G_1))
-ep = np.sqrt(newton())
-
-# f = 4 * n / ((n+1)**2 * np.exp(-1j*k0*n*d)-(n-1)**2 * np.exp(1j*k0*n*d))
-
-# plt.plot(omega, f.real)
-# plt.plot(omega, n.imag)
-# plt.show()
-# plt.plot(omega, (n**2).imag, "*--")
-# plt.plot(omega, (n**2).real, "*--")
+n = newton()**2
+print(n)
+ax[1,1].plot(omega[300:], abs(n.imag)[300:], "--")
+ax[1,1].plot(omega[300:], abs(n.real)[300:], "--")
 
 
-plt.plot(omega, ep.imag)
 plt.show()
+
--- 9.py
+++ 9.py
@@ -1,122 +1,57 @@
 import numpy as np
+from matplotlib.pyplot import *
 from matplotlib import pyplot as plt
-import math
-from matplotlib.animation import ArtistAnimation
-import scienceplots
-from numpy import loadtxt
-from scipy.fftpack import *
 
-plt.style.use(['science', 'notebook', 'grid', 'Solarize_Light2'])
+global c0, c1
+c0 = 1
+c1 = 0
 
-
-
-omega = np.linspace(0, 5e+12, 1024)
-wpl = 0.5e12
-G = 0.1e12
-w1 = 2*np.pi*1e12
-F1 = 1
-f0 = 1e12
-tp = 0.5e-12
-z = 1e-3
-d = 0.5e-3
-w0 = 2*np.pi*f0
-c = 3e8
-k0 = w0/c
-
-
-eps = 1e-7
-eps1 = 1e-9
-dc = 10 / 3
-f0 = 1.0
-tp = 0.5
-delf = 0.6
-fmin = f0 - delf
-fmax = f0 + delf
-z = 1.0
-omj = 2 * np.pi * 1.3
-ompl = 1.0
-gam = 0.5
-
-
-data = loadtxt(r"C:\Users\User\Desktop\epsdata.txt", comments="#", delimiter=" ", unpack=True)
-E = data[1]
-t = data[0] * 1e-12
-t_ = np.linspace(-10, 10, 1024)
-
-c = 3e+8
-
-
+global a1,a2
+a1 = 3
+a2 = 2
 def func(x):
-	return np.exp(-x**2 / (4 * tp**2)) * np.cos(w0 * x)
+    return np.cos(x) - c1*a1-c1*x*a2-c0*a2
+def K(s,t):
+    return a1+a2*(s-t)
+def volterra_it(n,x,y):
+    p=0
+    phi=0*np.array(x)
+    for i in range(n):
+        k=0
+        for j in range(i):
+            k+=K(x[i],y[j])*func(y[i])
+        phi[i]=-k*(x[-1]-x[1])/len(x)+func(x[i])
+    while p<50:
+        for i in range(n):
+            k=0
+            for j in range(i):
+                k+=K(x[i],y[j])*phi[j]
+            k*= (x[-1]-x[1])/len(x)
+            phi[i]=-k+func(x[i])
+        p+=1
+    return phi
 
+def volterra_test(n):
+    a = 0
+    b = 10
+    x = np.linspace(a,b,n)
+    y = np.linspace(a,b,n)
+    u = 0*np.array(x)
+    phi = volterra_it(n,x,y)
+    for i in range(n):
+        k=0
+        for j in range(i):
+            k += phi[j]*(x[i]-y[j])
+        u[i] = c0+c1*x[i]+k*(x[-1]-x[1])/len(x)
 
-# F = fft(func(t_))
-# plt.plot(t, E)
-# plt.show()
+    tochn = 0*np.array(x)
+    for i in range(n):
+            tochn[i] = -0.6*np.exp(-2*x[i])+1.5*np.exp(-x[i])+3*np.sin(x[i])/10+np.cos(x[i])/10
+    fig, ax = plt.subplots()
+    ax.plot(x,u,'--', label = 'Численное решение',color = 'green')
+    ax.plot(x,tochn,label = 'Точное решение', color = 'brown') #точное реш
+    ax.legend()
+    ax.grid()
+    plt.show()
 
-# plt.plot(omega, F)
-# plt.show()
-
-
-def T(w_):
-    dt = t[1]-t[0]
-    a = []
-    for i in range(t.shape[0]):
-        a.append(sum(np.exp(1j * w_[i] * (t - z / c)) * E) * dt)
-    ans = np.asarray(a)
-    return ans
-T = T(omega)
-
-
-plt.plot(omega, T.imag)
-plt.plot(omega, T.real)
-
-plt.show()
-
-def eps(w_):
-	w_pl = 1.5 * THz # THz
-	G = 0.5 * THz # THz
-	w1 = 1 * THz  # THz
-	eps_ = 1 + w_pl ** 2 * ((w1 ** 2 - w_ ** 2) / ((w1 ** 2 - w_ ** 2) ** 2 + w_ ** 2 * G ** 2) +
-	                        + 1j * (G * w_) / (((w1 ** 2 - w_ ** 2) ** 2) + w_ ** 2 * G ** 2))
-	# plt.plot(omega, eps_.real)
-	# plt.plot(omega, eps_.imag)
-	k0 = w_ / 3e+8
-	n = np.sqrt(eps_)
-	T = 4 * n / ((n+1)**2 * np.exp(-1j*k0*n*d)-(n-1)**2 * np.exp(1j*k0*n*d))
-	return n, T, k0
-
-
-# n, T_, k0 = eps(omega)
-# T = T(omega)
-# plt.plot(omega, T)
-# plt.plot(omega, T)
-# plt.show()
-def newton():
-	k0 = omega / 3e+8
-	def f(n_):
-		g = 4 * n_ / ((n_+1)**2 * np.exp(-1j*k0*n_*d)-(n_-1)**2 * np.exp(1j*k0*n_*d)) - T
-		return g
-	def df(n_, h = 0.001):
-		dg = (f(n_ + h) - f(n_)) / h
-		return dg
-	def iter(i = 0, xn = 1):
-		xn_ = xn - f(xn) / df(xn)
-		# print(xn_[0])
-		if i == 3:
-			return xn_
-		else:
-			i += 1
-			ans = iter(i, xn = xn_)
-			return ans
-		return xn_
-	ans = iter(0, xn = 1)
-	return ans
-
-# n_ = newton()
-#
-# plt.plot(omega, abs(n_.imag), "*")
-# plt.plot(omega, abs(n_.real), "*")
-# plt.plot(omega, abs(n.imag))
-# plt.plot(omega, abs(n.real))
-# plt.show()
+volterra_test(100)
